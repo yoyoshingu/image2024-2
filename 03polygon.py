f0d0d02e704e5b270fa8d1c7cf96f950e4864121
yoyoshingu <leo@shingu.ac.kr>
2024-09-05 16:51:54 +0900

    2nd commit

---
+++ 03polygon.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+#img = np.full((500,500,3), 255, dtype=np.uint8)
+
+img_file = "./img/girl.jpg"
+img = cv2.imread(img_file)
+
+cv2.rectangle(img, (50,50), (150,150), (255,0,0),10)
+cv2.rectangle(img, (300,300), (100,100), (0,255,0), 10)
+cv2.rectangle(img, (450,200), (200,450), (0,0,255), -1)
+cv2.imshow('polygon', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
