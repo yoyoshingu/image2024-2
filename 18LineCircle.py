60cf21a31b82e8092868b7b51b1c37bae99719de
yoyoshingu <leo@shingu.ac.kr>
2024-11-25 15:52:14 +0900

    HoughCircles

--- 18LineCircle.py
+++ 18LineCircle.py
@@ -3,8 +3,11 @@
 
 import cv2
 import numpy as np
+from cv2 import COLOR_BGR2GRAY
 
-img = cv2.imread('./img/sudoku.png')
+# 1. 허프변환을 이용한 직선검출 cv2.HoughLines()
+
+img = cv2.imread('./img/paper.jpg')
 # 컬러이미지는 흑백변환
 #img = cv2.imread("./img/children.jpg", cv2.IMREAD_GRAYSCALE)
 
@@ -28,5 +31,38 @@ if lines is not None:
 
 cv2.imshow('img', img)
 cv2.imshow('edges', edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# 2. 확률적 허프변환을 이용한 직선검출 cv2.HoughLinesP()
+
+
+img = cv2.imread('./img/paper.jpg')
+edges = cv2.Canny(img, 50, 150, apertureSize=3)
+linesp = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=20)
+
+print(linesp)
+if linesp is not None:
+    for line in linesp:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# 3 허프변환을 이용한 원 검출 cv2.HoughCircles()
+
+img = cv2.imread('./img/coins_connected.jpg')
+imgray = cv2.cvtColor(img, COLOR_BGR2GRAY)
+circles = cv2.HoughCircles(imgray, method=cv2.HOUGH_GRADIENT,
+                           dp=1, minDist=50, param2=120,
+                           minRadius=10, maxRadius=100)
+circles = np.int32(circles)
+for circle in circles[0, :]:
+    cx, cy, r = circle
+    cv2.circle(img, (cx, cy), r, (0,0,255), 2)
+
+cv2.imshow('img', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
